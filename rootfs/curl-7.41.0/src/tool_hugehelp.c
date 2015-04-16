b1b898237f6bc2e07c1b6bf2299888951ab62795
ljf10000 <l_j_f@163.com>
2015-04-16 17:32:38 -0400

    compile all

--- rootfs/curl-7.41.0/src/tool_hugehelp.c
+++ rootfs/curl-7.41.0/src/tool_hugehelp.c
@@ -2,7 +2,7 @@
 #ifndef HAVE_LIBZ
 /*
  * NEVER EVER edit this manually, fix the mkhelp.pl script instead!
- * Generation time: Wed Feb 25 00:06:10 2015
+ * Generation time: Wed Apr 15 16:15:10 2015
  */
 #ifdef USE_MANUAL
 #include "tool_hugehelp.h"
@@ -4341,7 +4341,7 @@ void hugehelp(void) {}
 #else
 /*
  * NEVER EVER edit this manually, fix the mkhelp.pl script instead!
- * Generation time: Wed Feb 25 00:06:10 2015
+ * Generation time: Wed Apr 15 16:15:10 2015
  */
 #ifdef USE_MANUAL
 #include "tool_hugehelp.h"
